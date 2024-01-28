134425c3c1cbc47b3bc46632e893d2e7a6029e06
username <wecp-dummy@gmail.com>
2024-01-28 10:37:08 +0000

    Day 5 task 2 test cases passed

--- backend/src/main/java/com/wecp/progressive/controller/CustomerController.java
+++ backend/src/main/java/com/wecp/progressive/controller/CustomerController.java
@@ -3,30 +3,90 @@ package com.wecp.progressive.controller;
 
 import com.wecp.progressive.entity.Customers;
 import com.wecp.progressive.entity.Transactions;
+import com.wecp.progressive.service.CustomerService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.SQLException;
 import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 
+
+
+@RestController
 public class CustomerController {
 
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("/customers")
     public ResponseEntity<List<Customers>> getAllCustomers() {
-        return null;
+        try
+        {
+            List<Customers> list = customerService.getAllCustomers();
+            return new ResponseEntity<>(list, HttpStatus.OK);
+        }
+        catch(SQLException e)
+        {
+            return null;
+        }
+        
     }
 
-    public ResponseEntity<Customers> getCustomerById(int customerId) {
-        return null;
+    @GetMapping("/customers/{customerId}")
+    public ResponseEntity<Customers> getCustomerById(@PathVariable int customerId) {
+        try 
+        {
+            Customers c = customerService.getCustomerById(customerId);
+            if(c == null)
+            {
+                return new ResponseEntity<Customers>(c, HttpStatus.NOT_FOUND);
+            }
+            else 
+            {
+                return new ResponseEntity<Customers>(c, HttpStatus.OK);
+            }
+
+        }
+        catch(SQLException e)
+        {
+            return null;
+        }
     }
 
     public ResponseEntity<Integer> addCustomer(Customers customers) {
-        return null;
+        try 
+        {
+            customerService.addCustomer(customers);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        
+        
     }
 
     public ResponseEntity<Void> updateCustomer(int customerId, Customers customers) {
         return null;
     }
     public ResponseEntity<Void> deleteCustomer(int customerId) {
+        try 
+        {
+            customerService.deleteCustomer(customerId);
+            return new ResponseEntity<Void>(HttpStatus.OK);
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
         return null;
+        
     }
 
     public ResponseEntity<List<Transactions>> getAllTransactionsByCustomerId(int cutomerId) {
--- backend/src/main/java/com/wecp/progressive/service/CustomerServiceImplJpa.java
+++ backend/src/main/java/com/wecp/progressive/service/CustomerServiceImplJpa.java
@@ -1,5 +1,101 @@
 package com.wecp.progressive.service;
 
-public class CustomerServiceImplJpa {
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import com.wecp.progressive.entity.Customers;
+import com.wecp.progressive.repository.CustomerRepository;
+
+@Service
+public class CustomerServiceImplJpa implements CustomerService{
     
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public List<Customers> getAllCustomers() throws SQLException {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'getAllCustomers'");
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Customers getCustomerById(int customerId) throws SQLException {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'getCustomerById'");
+        return null;
+    }
+
+    @Override
+    public int addCustomer(Customers customers) throws SQLException {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'addCustomer'");
+        customerRepository.save(customers);
+        return -1;
+    }
+
+    @Override
+    public void updateCustomer(Customers customers) throws SQLException {
+        // TODO Auto-generated method stub
+       // throw new UnsupportedOperationException("Unimplemented method 'updateCustomer'");
+
+    }
+
+    @Override
+    public void deleteCustomer(int customerId) throws SQLException {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'deleteCustomer'");
+        customerRepository.deleteById(customerId);
+    }
+
+    @Override
+    public List<Customers> getAllCustomersSortedByName() throws SQLException {
+        // TODO Auto-generated method stub
+       // throw new UnsupportedOperationException("Unimplemented method 'getAllCustomersSortedByName'");
+       List<Customers> sortedList = customerRepository.findAll();
+       Collections.sort(sortedList);
+       return sortedList;
+    }
+
+    @Override
+    public List<Customers> getAllCustomersFromArrayList() {
+        // TODO Auto-generated method stub
+       // throw new UnsupportedOperationException("Unimplemented method 'getAllCustomersFromArrayList'");
+       List<Customers> arr = customerRepository.findAll();
+       return arr;
+    }
+
+    @Override
+    public List<Customers> addCustomersToArrayList(Customers customers) {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'addCustomersToArrayList'");
+        Customers c = customerRepository.save(customers);
+        List<Customers> list = customerRepository.findAll();
+        return list;
+
+    }
+
+    @Override
+    public List<Customers> getAllCustomersSortedByNameFromArrayList() {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'getAllCustomersSortedByNameFromArrayList'");
+        List<Customers> list = customerRepository.findAll();
+        Collections.sort(list);
+        return list;
+    }
+
+    @Override
+    public void emptyArrayList() {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'emptyArrayList'");
+        
+    }
+
+    
+
 }
